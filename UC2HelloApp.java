336147e5223a14a9ab93a83b20dc12b42e3cf492
Raj Adithiyha <ra7511@srmist.edu.in>
2026-03-11 10:33:07 +0530

    Coded UC2: Display user name using command-line argument

---
+++ UC2HelloApp.java
@@ -0,0 +1,12 @@
+
+public class UC2HelloApp {
+
+    public static void main(String[] args) {
+
+       
+        String name = args[0];
+
+       
+        System.out.println("Hello, " + name + "!");
+    }
+}
